4d3b83fe9114c7e4a0bba83cdd063a0552c10209
Jayprajapati19 <jayp37679@gmail.com>
2025-12-19 19:17:26 +0530

    feat: implement user logout, email verification, and password management features

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 import { User } from "../models/user.models.js";
-import { ProjectMember } from "../models/projectmember.models.js";
+// import { ProjectMember } from "../models/projectmember.models.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import jwt from "jsonwebtoken";
@@ -29,34 +29,34 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
   }
 });
 
-export const validateProjectPermission = (roles = []) => {
-  asyncHandler(async (req, res, next) => {
-    const { projectId } = req.params;
+// export const validateProjectPermission = (roles = []) => {
+//   asyncHandler(async (req, res, next) => {
+//     const { projectId } = req.params;
 
-    if (!projectId) {
-      throw new ApiError(400, "project id is missing");
-    }
+//     if (!projectId) {
+//       throw new ApiError(400, "project id is missing");
+//     }
 
-    const project = await ProjectMember.findOne({
-      project: new mongoose.Types.ObjectId(projectId),
-      user: new mongoose.Types.ObjectId(req.user._id),
-    });
+//     const project = await ProjectMember.findOne({
+//       project: new mongoose.Types.ObjectId(projectId),
+//       user: new mongoose.Types.ObjectId(req.user._id),
+//     });
 
-    if (!project) {
-      throw new ApiError(400, "project not found");
-    }
+//     if (!project) {
+//       throw new ApiError(400, "project not found");
+//     }
 
-    const givenRole = project?.role;
+//     const givenRole = project?.role;
 
-    req.user.role = givenRole;
+//     req.user.role = givenRole;
 
-    if (!roles.includes(givenRole)) {
-      throw new ApiError(
-        403,
-        "You do not have permission to perform this action",
-      );
-    }
+//     if (!roles.includes(givenRole)) {
+//       throw new ApiError(
+//         403,
+//         "You do not have permission to perform this action",
+//       );
+//     }
 
-    next();
-  });
-};
+//     next();
+//   });
+// };
